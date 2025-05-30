abb7eeef08dc4b3c61f7af55fb3ca3254cbd577b
NguyenTranVietQuoc <168426526+NguyenTranVietQuoc@users.noreply.github.com>
2025-05-30 16:16:24 +0700

    CRUD City, JobTag, PaymentType, 2 package

--- Service/Models/CityModels/CityModel.cs
+++ Service/Models/CityModels/CityModel.cs
@@ -9,12 +9,8 @@ namespace Service.Models.CityModels
 {
     public class CityModel
     {
-        [Required(ErrorMessage = "Id is required.")]
         public int Id { get; set; }
-        [Required(ErrorMessage = "City name is required.")]
-        [StringLength(100, ErrorMessage = "City name cannot exceed 100 characters.")]
         public string CityName { get; set; }
-        [StringLength(100, ErrorMessage = "Country name cannot exceed 100 characters.")]
         public string Country { get; set; }
     }
 }
